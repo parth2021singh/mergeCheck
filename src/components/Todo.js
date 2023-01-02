153e1ef9f228e21910068cc40f0fa4c8e6c5b102
parth2021singh <parth_singh@persistent.com>
2023-01-02 15:42:06 +0530

    first commit

---
+++ src/components/Todo.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react'
+import {useDispatch} from "react-redux"
+import { addTodo,deleteTodo,removeTodo } from '../actions'
+import "./Todo.css"
+
+import { useSelector } from 'react-redux'
+
+const Todo = () => {
+
+
+  const [inputData,setInputData]=useState('')
+
+  const dispatch = useDispatch()
+  const list = useSelector(state=>state.todoReducer.list)
+
+  return (
+    <div>
+      <h2 >Todo List App</h2>
+      <input type="text" placeholder='Add items...'  value={inputData} onChange={(e)=>setInputData(e.target.value)}/>
+      <span>
+      <button onClick={()=> dispatch(addTodo(inputData),setInputData(""))}>Add</button>
+      </span>
+   
+   <div>
+   {list.map((item) => {
+    return( 
+      <div key={item.id}>
+       <h3 >{item.data}</h3>
+        <button onClick={()=> dispatch(deleteTodo(item.id))}>Delete</button>
+        </div>
+  )})}
+
+    </div>
+    </div>
+  )
+}
+
+export default Todo
